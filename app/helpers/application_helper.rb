a049576c4a3b5b4c0a9cdb34d67f983ebf0968d6
Dan Davies Brackett <dan@daviesbrackett.com>
2013-11-15 21:44:09 -0800

    bootstrap: basics.
    still lots to do, but this is enough to give a pretty good idea of
    what's coming.  Signup form, login form, nav are done; remaining actual
    like, functionality still needs lipstick.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+	class BootstrapFormBuilder < ActionView::Helpers::FormBuilder
+		def field_helper(attribute, options={}, &basetag)
+			label(attribute, options[:label], {class:"col-sm-3 control-label"}) + 
+				@template.content_tag(:div,	(yield attribute, options.merge({class:"form-control",label:nil})), :class => "col-sm-9")
+		end
+		def text_field(attribute, options={})
+			field_helper(attribute, options) do |attrib, opts| super(attrib, opts) end
+		end
+		def password_field(attribute, options={})
+			field_helper(attribute, options) do |attrib, opts| super(attrib, opts) end
+		end
+		def submit(options={})
+			super(options.merge(:class => "btn btn-primary"))
+		end
+	end
+
+	@@formopts = {builder: BootstrapFormBuilder, html: {role:"form", class:"form-horizontal"}}
+
+	def bootstrap_form_for(record, options={}, &block)
+		form_for(record, options.merge(@@formopts), &block)
+	end
 end
