54de437b6bfad326f7c632bffc2f556d49c6b92c
Dan Davies Brackett <dan@daviesbrackett.com>
2013-11-18 00:41:45 -0800

    bootstrapify new game form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,24 @@
 module ApplicationHelper
+	
 	class BootstrapFormBuilder < ActionView::Helpers::FormBuilder
 		def field_helper(attribute, options={}, &basetag)
 			label(attribute, options[:label], {class:"col-sm-3 control-label"}) + 
-				@template.content_tag(:div,	(yield attribute, options.merge({class:"form-control",label:nil})), :class => "col-sm-9")
+				@template.content_tag(:div, (yield attribute, options.merge({class:"form-control", label:nil})), :class => "col-sm-9")
 		end
 		def text_field(attribute, options={})
 			field_helper(attribute, options) do |attrib, opts| super(attrib, opts) end
 		end
 		def password_field(attribute, options={})
 			field_helper(attribute, options) do |attrib, opts| super(attrib, opts) end
+		end
+		def number_field(attribute, options={})
+			field_helper(attribute, options) do |attrib, opts| super(attrib,opts) end
+		end
+		def check_box(attribute, options={})
+			@template.content_tag(:div, (
+					super(attribute, options) + " " +
+					label(attribute, options[:label], {class:"control-label"})
+				), {class:"col-sm-9 col-sm-offset-3 text-left"})
 		end
 		def submit(options={})
 			super(options.merge(:class => "btn btn-primary"))
@@ -19,5 +29,11 @@ module ApplicationHelper
 
 	def bootstrap_form_for(record, options={}, &block)
 		form_for(record, options.merge(@@formopts), &block)
+	end
+
+	def select(record, attribute, values, options={}, html_options={})
+		# hating this ugly stringification here - gotta be a railsy way to get at the default idification of a record attribute
+		label_tag( record.to_s + "_" + attribute.to_s, options[:label], {class:"col-sm-3 control-label"}) + 
+				content_tag(:div, super(record, attribute, values, options, html_options.merge({class:"form-control", label:nil})), :class => "col-sm-9")
 	end
 end
